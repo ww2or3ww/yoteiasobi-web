8eb4bd7d6f82628e5e2190ba5d97694b1865d706
ww2or3ww <w2or3w@gmail.com>
2021-01-24 00:05:28 +0000

    use env

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import Amplify from 'aws-amplify'
-import awsconfig from '@/src/aws-exports'
+//import awsconfig from '@/src/aws-exports'
+const awsconfig = {
+    "aws_project_region": "ap-northeast-1",
+    "aws_cognito_identity_pool_id": "",
+    "aws_cognito_region": "ap-northeast-1",
+    "aws_user_pools_id": "",
+    "aws_user_pools_web_client_id": "",
+    "oauth": {
+        "domain": "",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "",
+        "redirectSignOut": "",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS"
+};
 
-awsconfig.oauth.redirectSignIn = `${window.location.origin}/`;
-awsconfig.oauth.redirectSignOut = `${window.location.origin}/`;
-    
+awsconfig.aws_cognito_identity_pool_id = process.env.ENV_VAL_aws_cognito_identity_pool_id;
+awsconfig.aws_user_pools_id = process.env.ENV_VAL_aws_user_pools_id;
+awsconfig.aws_user_pools_web_client_id = process.env.ENV_VAL_aws_user_pools_web_client_id;
+awsconfig.oauth.domain = process.env.ENV_VAL_oauth_domain;
+awsconfig.oauth.redirectSignIn = process.env.ENV_VAL_oauth_redirectSignIn;
+awsconfig.oauth.redirectSignOut = process.env.ENV_VAL_oauth_redirectSignOut;
 Amplify.configure(awsconfig)
+
 Vue.use(Amplify)
