639cfd363b5df041ab9b1ad9d7bf89ea8db7f8d5
ww2or3ww <w2or3w@gmail.com>
2021-01-31 03:42:41 +0000

    refactoring

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <v-btn @click="signin">Sign In</v-btn>
+    <v-btn @click="signin('Google')">Google</v-btn>
+    <v-btn @click="signin('LINE')">LINE</v-btn>
   </div>
 </template>
 <script>
 import { Auth } from 'aws-amplify'
 export default {
   methods: {
-    signin() {
-      Auth.federatedSignIn({ provider: 'Google' })
+    signin(providerName) {
+      Auth.federatedSignIn({ provider: providerName })
     }
   }
 }
