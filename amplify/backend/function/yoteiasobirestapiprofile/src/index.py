1ef618a688c12529d30d2437f8afd74ff8275c72
ww2or3ww <w2or3w@gmail.com>
2021-02-21 03:18:58 +0000

    remove old image

--- amplify/backend/function/yoteiasobirestapiprofile/src/index.py
+++ amplify/backend/function/yoteiasobirestapiprofile/src/index.py
@@ -1,3 +1,4 @@
+import os
 import json
 import datetime
 
@@ -7,6 +8,9 @@ logger.setLevel(logging.INFO)
 
 import boto3
 COGNITO_CLIENT = boto3.client('cognito-idp')
+S3 = boto3.client("s3")
+
+S3_BUCKET_NAME   = os.environ['S3_BUCKET_NAME']
 
 def handler(event, context):
   try:
@@ -17,10 +21,13 @@ def handler(event, context):
     name, comment, picture = getParamFromBody(body)
     logger.info('name={0}, comment={1}, picture={2}'.format(name, comment, picture))
 
-    poolId, userName = getUserInfo(event)
-    logger.info('user info = {0}, {1}'.format(poolId, userName))
+    poolId, userName, pictureOrg = getUserInfo(event)
+    logger.info('user info = {0}, {1}, {2}'.format(poolId, userName, pictureOrg))
 
     updateUserInfo(poolId, userName, name, comment, picture)
+    
+    if picture and pictureOrg:
+      removeOldPicture(pictureOrg)
     
     return {
       "statusCode": 200,
@@ -62,6 +69,7 @@ def getParamFromBody(body):
 def getUserInfo(event):
   poolId = None
   userName = None
+  pictureOrg = None
 
   try:
     poolId = event["requestContext"]["identity"]["cognitoAuthenticationProvider"].split(',')[0].split('/')[1]
@@ -71,13 +79,22 @@ def getUserInfo(event):
         Filter = "sub = \"{0}\"".format(userSub)
     )
     userName = response['Users'][0]['Username']
+    attributes = response['Users'][0]['Attributes']
+    pictureOrg = get_value_from_attributes(attributes, 'custom:picture')
     
   except Exception as e:
     logger.exception(e)
     raise
 
-  return poolId, userName
+  return poolId, userName, pictureOrg
 
+def get_value_from_attributes(attributes, name):
+  attr = list(filter(lambda data: data['Name'] == name , attributes))
+  if attr and len(attr) == 1:
+    return attr[0]['Value']
+  
+  return ''
+  
 def updateUserInfo(poolId, userName, name, comment, picture):
 
   try:
@@ -109,3 +126,9 @@ def updateUserInfo(poolId, userName, name, comment, picture):
   except Exception as e:
     logger.exception(e)
     raise
+
+def removeOldPicture(picture):
+  try:
+    S3.delete_object(Bucket=S3_BUCKET_NAME, Key="public/{0}".format(picture))
+  except Exception as e:
+    logger.exception(e)
