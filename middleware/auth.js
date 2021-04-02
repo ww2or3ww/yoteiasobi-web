4aadaaa73998ec47f1c00275b8b93a380d058a12
ww2or3ww <w2or3w@gmail.com>
2021-04-02 14:38:13 +0000

    calendar

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,11 @@
 import { Auth, Storage } from 'aws-amplify'
 
-const NEED_AUTHENTICATED_PAGES  = ['/user', '/profile', '/userManage', '/userProfile', '/calendarSearch']
-const NEED_ADMIN_PAGES          = ['/userManage', '/userProfile']
+const NEED_AUTHENTICATED_PAGES  = ['/profile', '/users']
+const NEED_ADMIN_PAGES          = ['/users']
 
 export default async (context) => {
+  console.log(">>>>")
+  console.log(context.route.path)
   if (context.store.state.authdata.user == null) {
     let user = await Auth.currentUserInfo()
     try {
@@ -16,12 +18,12 @@ export default async (context) => {
       user = null
     }
     if (user == null) {
-      if (NEED_AUTHENTICATED_PAGES.indexOf(context.route.path) >= 0) {
+      if (NEED_AUTHENTICATED_PAGES.indexOf(context.route.path) == 0) {
         context.redirect('/login')
       }
     }
   } else {
-    if (NEED_ADMIN_PAGES.indexOf(context.route.path) >= 0) {
+    if (NEED_ADMIN_PAGES.indexOf(context.route.path) == 0) {
       const isAdmin = context.$auth_is_admin()
       if (isAdmin != "1") {
         context.redirect('/')
