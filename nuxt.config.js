da9ff352e913877989d891764fc6a9f8f3cfc900
ww2or3ww <w2or3w@gmail.com>
2021-04-04 14:55:39 +0000

    calendar

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,6 +52,14 @@ export default {
         type: 'image/x-icon', 
         href: '/img/favicon.png'
       },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"57x57",    href: `/img/apple-touch-icon-57x57.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"72x72",    href: `/img/apple-touch-icon-72x72.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"76x76",    href: `/img/apple-touch-icon-76x76.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"114x114",  href: `/img/apple-touch-icon-114x114.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"120x120",  href: `/img/apple-touch-icon-120x120.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"144x144",  href: `/img/apple-touch-icon-144x144.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"152x152",  href: `/img/apple-touch-icon-152x152.png` },
+      { rel: 'apple-touch-icon', type: 'image/x-icon',sizes:"180x180",  href: `/img/apple-touch-icon-180x180.png` },
       {
         rel: 'stylesheet', 
         href: 'https://fonts.googleapis.com/css2?family=Train%20One'
